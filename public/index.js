51ae506db0f8df99f7c403959660018e419e6639
unknown <azsa.nwhy@gmail.com>
2021-08-17 20:50:48 +0700

    menambahkan countdown deadline tugas

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+// Digital Clock
 function clock() {
   var hours = document.getElementById("hours");
   var minutes = document.getElementById("minutes");
@@ -56,3 +57,82 @@ function clock() {
   seconds.innerHTML = s;
 }
 var interval = setInterval(clock, 1000);
+
+// Countdown1 PKWU
+
+var countDate = new Date('August 22 2021 00:00:00').getTime();
+
+function CountdownPKWU() {
+  var now = new Date().getTime();
+  gap = countDate - now
+
+  var detik = 1000;
+  var menit = detik * 60;
+  var jam = menit * 60;
+  var hari = jam * 24;
+  
+  var h = Math.floor(gap / (hari));
+  var j = Math.floor((gap % (hari)) / (jam));
+  var m = Math.floor((gap % (jam)) / (menit));
+  var d = Math.floor((gap % (menit) / (detik)));
+
+  document.getElementById('hari').innerText = h;
+  document.getElementById('jam').innerText = j;
+  document.getElementById('menit').innerText = m;
+  document.getElementById('detik').innerText = d;
+}
+setInterval( function(){
+  CountdownPKWU();
+}, 1000);
+
+// Countdown2 Geografi
+var countDate2 = new Date('August 19 2021 18:00:00').getTime();
+
+function CountdownGeo() {
+  var now = new Date().getTime();
+  gap = countDate2 - now
+
+  var detik2 = 1000;
+  var menit2 = detik2 * 60;
+  var jam2 = menit2 * 60;
+  var hari2 = jam2 * 24;
+  
+  var h2 = Math.floor(gap / (hari2));
+  var j2 = Math.floor((gap % (hari2)) / (jam2));
+  var m2 = Math.floor((gap % (jam2)) / (menit2));
+  var d2 = Math.floor((gap % (menit2) / (detik2)));
+
+  document.getElementById('hari2').innerText = h2;
+  document.getElementById('jam2').innerText = j2;
+  document.getElementById('menit2').innerText = m2;
+  document.getElementById('detik2').innerText = d2;
+}
+setInterval( function(){
+  CountdownGeo();
+}, 1000);
+
+// Countdown Matematika
+var countDate3 = new Date('August 19 2021 23:59:59').getTime();
+
+function CountdownMtk() {
+  var now = new Date().getTime();
+  gap = countDate3 - now
+
+  var detik3 = 1000;
+  var menit3 = detik3 * 60;
+  var jam3 = menit3 * 60;
+  var hari3 = jam3 * 24;
+  
+  var h3 = Math.floor(gap / (hari3));
+  var j3 = Math.floor((gap % (hari3)) / (jam3));
+  var m3 = Math.floor((gap % (jam3)) / (menit3));
+  var d3 = Math.floor((gap % (menit3) / (detik3)));
+
+  document.getElementById('hari3').innerText = h3;
+  document.getElementById('jam3').innerText = j3;
+  document.getElementById('menit3').innerText = m3;
+  document.getElementById('detik3').innerText = d3;
+}
+setInterval( function(){
+  CountdownMtk();
+}, 1000);
