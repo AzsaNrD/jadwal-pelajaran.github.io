855a245228e92a333840b586563fbfd1cf6aa288
unknown <azsa.nwhy@gmail.com>
2021-08-21 09:17:54 +0700

    particle js

--- public/index.js
+++ public/index.js
@@ -101,38 +101,6 @@ setInterval( function(){
   CountdownPKWU();
 }, 1000);
 
-// Countdown3 Matematika
-var countDate3 = new Date('August 20 2021 23:59:00').getTime();
-
-function CountdownMtk() {
-  var now = new Date().getTime();
-  gap = countDate3 - now
-
-  var detik3 = 1000;
-  var menit3 = detik3 * 60;
-  var jam3 = menit3 * 60;
-  var hari3 = jam3 * 24;
-  
-  var h3 = Math.floor(gap / (hari3));
-  var j3 = Math.floor((gap % (hari3)) / (jam3));
-  var m3 = Math.floor((gap % (jam3)) / (menit3));
-  var d3 = Math.floor((gap % (menit3) / (detik3)));
-
-  document.getElementById('hari3').innerText = h3;
-  document.getElementById('jam3').innerText = j3;
-  document.getElementById('menit3').innerText = m3;
-  document.getElementById('detik3').innerText = d3;
-
-  if (gap < 0) {
-    clearInterval(CountdownMtk);
-    document.getElementById("countdown3").innerHTML = "";
-    document.getElementById("timeOutMtk").innerHTML = "Telah lewat tenggat waktu.";
-  }
-}
-setInterval( function(){
-  CountdownMtk();
-}, 1000);
-
 // Countdown4 Sejindo
 var countDate4 = new Date('August 25 2021 19:00:00').getTime();
 
@@ -163,39 +131,6 @@ function CountdownSj() {
 }
 setInterval( function(){
   CountdownSj();
-}, 1000);
-
-
-// Countdown5 sunda
-var countDate5 = new Date('August 20 2021 12:00:00').getTime();
-
-function CountdownBs() {
-  var now = new Date().getTime();
-  gap = countDate5 - now
-
-  var detik5 = 1000;
-  var menit5 = detik5 * 60;
-  var jam5 = menit5 * 60;
-  var hari5 = jam5 * 24;
-  
-  var h5 = Math.floor(gap / (hari5));
-  var j5 = Math.floor((gap % (hari5)) / (jam5));
-  var m5 = Math.floor((gap % (jam5)) / (menit5));
-  var d5 = Math.floor((gap % (menit5) / (detik5)));
-
-  document.getElementById('hari5').innerText = h5;
-  document.getElementById('jam5').innerText = j5;
-  document.getElementById('menit5').innerText = m5;
-  document.getElementById('detik5').innerText = d5;
-
-  if (gap < 0) {
-    clearInterval(CountdownBs);
-    document.getElementById("countdown5").innerHTML = "";
-    document.getElementById("timeOutBs").innerHTML = "Telah lewat tenggat waktu.";
-  }
-}
-setInterval( function(){
-  CountdownBs();
 }, 1000);
 
 // Countdown6 Olahraga
